6ecbbda660ecbc3861a07a9cc9b5f88a2a659978
Yuan Yang <136157134+yuanyang0563@users.noreply.github.com>
2025-08-29 21:59:08 +0800

    Add files via upload

---
+++ shared control/teleoperation/gen3/include/manipulator.hpp
@@ -0,0 +1,48 @@
+#include "common.hpp"
+
+class manipulator {
+
+    public:
+    
+    	rclcpp::Node::SharedPtr node;    	    	    	
+    	
+    	explicit manipulator (const std::string& name);
+    	
+    	void getPose();
+    	void getFeatures();
+    	void setTwist();
+    	void storeData();
+    
+    private:
+    	    	
+    	rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_vel;
+    	rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr sub_js;
+    	rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub_img;
+    	Eigen::VectorXf a, alpha, d, theta;
+    	Eigen::Vector3f xco, xec, xe, xc, x0, xd;
+    	Eigen::Matrix3f Rco, Rec, Re, Rc, R0, Rd;
+    	Eigen::VectorXf q, s, sd, twist;
+    	Eigen::MatrixXf L, J, Tec, Tco;
+    	Eigen::Vector3f ncd, mcd;
+    	Eigen::Matrix3f Hc;
+    	std::vector<Eigen::Vector3f> xl;
+    	std::vector<Eigen::Matrix3f> Rl;
+    	
+    	std::vector<std::vector<vpImagePoint>> tagCorners_traj;
+    	std::vector<vpImagePoint> tagCorners, tagCorners0;
+    	std::vector<std::vector<double>> p, pd;
+    	vpImage<unsigned char> image;
+    	vpDetectorAprilTag detector;
+    	vpCameraParameters camera;
+    	vpHomogeneousMatrix cMo;
+    	vpDisplayX display;
+    	
+    	std::stringstream file_name;
+    	std::vector<float*> data;
+    	
+    	std::chrono::time_point<std::chrono::steady_clock> t_prev, t_curr;
+    	int waypoint;
+    	
+    	void joint_state_callback (const sensor_msgs::msg::JointState::SharedPtr msg);
+    	void camera_image_callback (const sensor_msgs::msg::Image::SharedPtr msg);
+};
---
+++ shared control/teleoperation/gen3/src/common.cpp
@@ -0,0 +1,29 @@
+#include "common.hpp"
+
+
+bool flag_js = false;
+bool flag_at = false;
+bool flag_init = false;
+bool flag_data = false;
+float tagSize = 0.04;
+float lambda_ibvs = 0.35;
+float lambda_pbvs = 0.75;
+float lambda_hbvs = 0.10;
+std::string mode;
+std::atomic_bool flag_stop = false;
+
+void signalHandler (int signum) {
+    	flag_stop = true;
+}
+
+Eigen::Vector3f skewVec (const Eigen::Matrix3f& R) {
+	Eigen::Matrix3f S = 0.5*(R-R.transpose());
+	Eigen::Vector3f v(S(2,1),S(0,2),S(1,0));
+	return v;
+}
+
+Eigen::Matrix3f skewMat (const Eigen::Vector3f& v) {
+	Eigen::Matrix3f R;
+	R << 0.0, -v(2), v(1), v(2), 0.0, -v(0), -v(1), v(0), 0.0;
+	return R;
+}
